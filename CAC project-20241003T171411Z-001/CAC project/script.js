c80f61bd1adf38fd7fff9b0473fe4bb7d47c0542
Akosa123 <125832573+Akosa123@users.noreply.github.com>
2024-10-03 12:38:17 -0500

    Add files via upload

---
+++ CAC project-20241003T171411Z-001/CAC project/script.js
@@ -0,0 +1,50 @@
+let selectedButton = null;
+let indexNum = 0; 
+// get current question, be able to change question
+const resultBar = document.getElementById("result-bar");
+const question = document.getElementById("question");
+const quizClass = document.querySelector(".quiz");
+const questionList = ["Test Question 1", "Test Question 2", "Test Question 3"];
+let questionNum = 0; 
+let score = 0; 
+
+question.textContent = questionList[0];
+
+document.querySelectorAll(".btn").forEach((button, index) => {
+    button.addEventListener('click', event => {
+        if (selectedButton) {
+            selectedButton.classList.remove('selected');
+        }
+
+        button.classList.add('selected');
+        selectedButton = button;
+        
+        indexNum = index + 1;  
+    })
+})
+
+// on next_button click, call function next() 
+document.getElementById('next_button').addEventListener('click', () => {
+    if (indexNum == 1){
+        score += 3; 
+        questionNum++;
+    } else if (indexNum == 2){
+        score += 2;
+        questionNum++;
+    } else if (indexNum == 3){
+        score += 1;
+        questionNum++;
+    } else if (indexNum == 4){
+        questionNum++;
+    } else {
+        console.log("no button selected")
+    }
+    question.textContent = questionList[questionNum];
+    document.querySelectorAll(".btn").forEach(button =>{
+        button.classList.remove('selected');
+    })
+    if (questionNum == questionList.length){
+        window.location.href = "/results.html";
+    }
+   
+})
