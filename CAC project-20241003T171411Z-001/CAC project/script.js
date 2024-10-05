34fa01e04159aebff479bfa9d63bb85ffa472c1b
Ryantrash <ryandherber@gmail.com>
2024-10-04 20:42:40 -0500

    Comments + TODOs
    Added comments + TODOs for the code
    
    General TODO: we need cleaner code? Anyone pretty please :heart_eyes:

--- CAC project-20241003T171411Z-001/CAC project/script.js
+++ CAC project-20241003T171411Z-001/CAC project/script.js
@@ -1,28 +1,36 @@
+// button "selected" functionality
 let selectedButton = null;
 let indexNum = 0; 
-// get current question, be able to change question
+
+// question functionality + question List (edit this for adding more questions) 
+// + score var TODO: Add more questions / add real questions
 const question = document.getElementById("question");
 const questionList = ["Test Question 1", "Test Question 2", "Test Question 3"];
 let questionNum = 0; 
 let score = 0; 
 
+// initialize questions
 question.textContent = questionList[0];
 
+// button select function -- Parameters{button: each button in class btn,
+// index: index of current button}
 document.querySelectorAll(".btn").forEach((button, index) => {
     button.addEventListener('click', event => {
+        // if button already selected, make sure to remove that button on new click
         if (selectedButton) {
             selectedButton.classList.remove('selected');
         }
-
+        // if button is clicked, add class "selected"
         button.classList.add('selected');
         selectedButton = button;
-        
+        // iterate to which button is selected
         indexNum = index + 1;  
     })
 })
 
-// on next_button click, call function next() 
+// on next_button click, call function
 document.getElementById('next_button').addEventListener('click', () => {
+    // determine which score to add based on current button selected
     if (indexNum == 1){
         score += 3; 
         questionNum++;
@@ -37,17 +45,26 @@ document.getElementById('next_button').addEventListener('click', () => {
     } else {
         console.log("no button selected")
     }
+    // iterate to next question in list
     question.textContent = questionList[questionNum];
+
+    // remove selected class from all buttons
     document.querySelectorAll(".btn").forEach(button =>{
         button.classList.remove('selected');
     })
+    // if questionNum is final question in list
     if (questionNum == questionList.length){
-        console.log("switch")
+        // get all result + quiz elements to change 
         let quiz = document.getElementById("quiz");
         let results = document.getElementById("results");
         let resultsImage = document.getElementById("results-image");
         let resultsText = document.getElementById("results-text");
+        // hide quiz div
         quiz.style.display = "none";
+
+        // determine which image to display based on score
+        // EDIT IF YOU ADD MORE QUESTIONS
+        // TODO: Change placeholders + add more scores 
         if(score == 9){
             resultsImage.src = "images/100_chart.png";
             resultsText.textContent = "PLACEHOLDER 100%";
@@ -58,8 +75,8 @@ document.getElementById('next_button').addEventListener('click', () => {
             resultsImage.src = "images/33_chart.png";
             resultsText.textContent = "PLACEHOLDER 33%";
         }
+        // show results div
         results.style.display = "block";
 
     }
-    console.log(score);
 })
