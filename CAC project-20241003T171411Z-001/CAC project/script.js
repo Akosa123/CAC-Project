de50ec0de9cba0498dd23d39dfc26bcfc1699449
Ryantrash <ryandherber@gmail.com>
2024-10-19 14:28:43 -0500

    Charts and JS imp.
    Implemented the chart images current and implemented them in js
    
    also changed around some js to make more sense

--- CAC project-20241003T171411Z-001/CAC project/script.js
+++ CAC project-20241003T171411Z-001/CAC project/script.js
@@ -28,44 +28,28 @@ document.querySelectorAll(".btn").forEach((button, index) => {
     });
 });
 
-function updateProgressDial(scorePercentage) {
-    const circle = document.querySelector('.progress-ring__circle');
-    const radius = circle.r.baseVal.value;
-    const circumference = Math.PI * radius;
-
-    circle.style.strokeDasharray = `${circumference} ${circumference}`;
-    const offset = circumference - (scorePercentage / 100) * circumference;
-    circle.style.strokeDashoffset = offset;
-
-    const needle = document.querySelector('.needle');
-    const needleRotation = (-90 + (scorePercentage / 100) * 180); 
-    needle.style.transform = `rotate(${needleRotation}deg)`;
-}
-
 // on next_button click, call function
 document.getElementById('next_button').addEventListener('click', () => {
     // determine which score to add based on current button selected
-    if (indexNum == 1){
-        score += 3; 
-        questionNum++;
-    } else if (indexNum == 2){
-        score += 2;
-        questionNum++;
-    } else if (indexNum == 3){
-        score += 1;
-        questionNum++;
-    } else if (indexNum == 4){
-        questionNum++;
-    } else {
-        console.log("no button selected");
+    if(indexNum != null){
+        if (indexNum == 1){
+            score += 3; 
+            questionNum++;
+        } else if (indexNum == 2){
+            score += 2;
+            questionNum++;
+        } else if (indexNum == 3){
+            score += 1;
+            questionNum++;
+        } else{
+            questionNum++;
+        }
+        indexNum = null;
     }
-    // iterate to next question in list
     question.textContent = questionList[questionNum];
+    
+    selectedButton.classList.remove('selected')
 
-    // remove selected class from all buttons
-    document.querySelectorAll(".btn").forEach(button =>{
-        button.classList.remove('selected');
-    })
     // if questionNum is final question in list
     if (questionNum == questionList.length){
         // get all result + quiz elements to change 
@@ -73,31 +57,60 @@ document.getElementById('next_button').addEventListener('click', () => {
         let results = document.getElementById("results");
         let resultsImage = document.getElementById("results-image");
         let resultsText = document.getElementById("results-text");
+
         // hide quiz div
         quiz.style.display = "none";
-
-        // determine which image to display based on score
-        // EDIT IF YOU ADD MORE QUESTIONS
-        // TODO: Change placeholders + add more scores 
-        if(score == 9){
-            resultsImage.src = "images/100_chart.png";
-            resultsText.textContent = "PLACEHOLDER 100%";
-        } else if(score > 6){
-            resultsImage.src = "images/66_chart.png";
-            resultsText.textContent = "PLACEHOLDER 66%";
+        
+        // display image based on scorePercentage TODO: add text
+        let scorePercentage = (score/(questionList.length*3))*100;
+        if(scorePercentage > 98){
+            resultsImage.src = "images/100chart.png";
+            resultsText.textContent = "placeholder 100%";
+        } else if(scorePercentage > 91){
+            resultsImage.src = "images/91chart.png";
+            resultsText.textContent = "placeholder 91%";
+        } else if(scorePercentage > 84){
+            resultsImage.src = "images/84chart.png";
+            resultsText.textContent = "placeholder 84%";
+        } else if(scorePercentage > 77){
+            resultsImage.src = "images/77chart.png";
+            resultsText.textContent = "placeholder 77%";
+        } else if(scorePercentage > 70){
+            resultsImage.src = "images/70chart.png";
+            resultsText.textContent = "placeholder 70%";
+        } else if(scorePercentage > 63){
+            resultsImage.src = "images/63chart.png";
+            resultsText.textContent = "placeholder 63%";
+        } else if(scorePercentage > 56){
+            resultsImage.src = "images/56chart.png";
+            resultsText.textContent = "placeholder 56%";
+        } else if(scorePercentage > 49){
+            resultsImage.src = "images/49chart.png";
+            resultsText.textContent = "placeholder 49%";
+        } else if(scorePercentage > 42){
+            resultsImage.src = "images/42chart.png";
+            resultsText.textContent = "placeholder 42%";
+        } else if(scorePercentage > 35){
+            resultsImage.src = "images/35chart.png";
+            resultsText.textContent = "placeholder 35%";
+        } else if(scorePercentage > 28){
+            resultsImage.src = "images/28chart.png";
+            resultsText.textContent = "placeholder 28%";
+        } else if(scorePercentage > 21){
+            resultsImage.src = "images/21chart.png";
+            resultsText.textContent = "placeholder 21%";
+        } else if(scorePercentage > 14){
+            resultsImage.src = "images/14chart.png";
+            resultsText.textContent = "placeholder 14%";
+        } else if(scorePercentage > 7){
+            resultsImage.src = "images/7chart.png";
+            resultsText.textContent = "placeholder 7%";
         } else {
-            resultsImage.src = "images/33_chart.png";
-            resultsText.textContent = "PLACEHOLDER 33%";
+            resultsImage.src = "images/0chart.png";
+            resultsText.textContent = "placeholder 0%";
         }
+
         // show results div
         results.style.display = "block";
-
-        let scorePercentage = (score / (questionList.length * 3)) * 100;
-        updateProgressDial(scorePercentage);
-    } else {
-        question.textContent = questionList[questionNum];
-        document.querySelectorAll(".btn").forEach(button => {
-            button.classList.remove('selected');
-        });
     }
 })
